3eb814a446c5867f3d064ffee1a390172b04254b
Anoop555777 <ajbisht99@gmail.com>
2022-09-03 23:18:18 +0530

    form input

--- script.js
+++ script.js
@@ -13,8 +13,11 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
   #map;
+  #mapEvent;
   constructor() {
     this._getGeolocation();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElementField);
   }
 
   _getGeolocation() {
@@ -38,7 +41,42 @@ class App {
     this.#map.on('click', this._showForm.bind(this));
   }
 
-  _showForm() {}
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElementField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          maxHeight: 100,
+          autoClose: false,
+          closeOnClick: true,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
 
 const mapty = new App();
@@ -49,8 +87,3 @@ const mapty = new App();
 //   attribution:
 //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 // }).addTo(map);
-
-// L.marker([51.5, -0.09])
-//   .addTo(map)
-//   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//   .openPopup();
