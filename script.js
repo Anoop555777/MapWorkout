57782a08b72b9e865fe9c277fb97ea042992874a
Anoop555777 <ajbisht99@gmail.com>
2022-09-03 21:14:50 +0530

    map render to UI

--- script.js
+++ script.js
@@ -11,6 +11,46 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-class App {}
+class App {
+  #map;
+  constructor() {
+    this._getGeolocation();
+  }
+
+  _getGeolocation() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          console.log("can't find the location");
+        }
+      );
+  }
+
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    this.#map = L.map('map').setView([latitude, -longitude], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm() {}
+}
 
 const mapty = new App();
+
+// var map = ;
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution:
+//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// }).addTo(map);
+
+// L.marker([51.5, -0.09])
+//   .addTo(map)
+//   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//   .openPopup();
