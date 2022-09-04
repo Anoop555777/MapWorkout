66afcd5086307f089c87968f7eccfaa63b842b8f
Anoop555777 <ajbisht99@gmail.com>
2022-09-04 14:31:33 +0530

    class running  add

--- script.js
+++ script.js
@@ -37,6 +37,23 @@ class Workout {
   }
 }
 
+class Running extends Workout {
+  type = 'running';
+
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+    this._setDescription();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
 class App {
   #map;
   #mapEvent;
